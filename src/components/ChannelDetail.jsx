a99e2605cabf232bf52899c7e60c072419d79dfd
inder <inderpreet0639.be21@chitkara.edu.in>
2023-11-08 14:52:32 +0530

    ChannelDetailsCreated

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { useState,useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { Box } from "@mui/material"
+import {Videos,ChanneCard} from './'
+import { fetchFromAPI } from "../utils/fetchFromAPI"
 
 const ChannelDetail = () => {
+  const [channelDetail,setChannelDetail]=useState(null)
+  const{id}=useParams();
+  // console.log(channelDetail)
+  useEffect(()=>{
+    fetchFromAPI(`channels?part="snippet&id=${id}`)
+    .then((data)=> setChannelDetail(data?.items )) 
+    fetchFromAPI(`channel?part="snippet&id=${id}`)
+    .then((data)=> setChannelDetail(data?.items )) 
+  },[id])
   return (
-    <div>
+    <Box>
       
-    </div>
+    </Box>
   )
 }
 
